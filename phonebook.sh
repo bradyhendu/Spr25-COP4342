ecd3586d3b8036eaebcdc9522b3f5d69c8bfff04
Brady Henderson <bradyhendu@gmail.com>
2025-01-30 08:34:16 -0500

    epic

--- phonebook.sh
+++ phonebook.sh
@@ -50,12 +50,111 @@ else
             echo "Error: Record already exists in phonebook.dat"
             exit 1
         fi
+        #Assure that number fits the form ###-###-####
+        if ! echo $number | grep -Eq '^[0-9]{3}-[0-9]{3}-[0-9]{4}$'
+        then
+            echo "Error: Phone number must be in the form ###-###-####"
+            exit 1
+        fi
         echo "$last $first $number $address " >> phonebook.dat
         # Sort the phonebook.dat file, first priority is last name, second priority is first name
         sort +0 -1 +1 -2 phonebook.dat -o phonebook.dat
         exit 0
     fi
 fi
+
+# If the user has chosen to delete a record, ensure that there is enough information to do so <last> <first>
+if [ "$1" = "-d" ] && [ $# -lt 3 ]
+then
+    echo "Error: Inappropriate use of phonebook.sh, not enough arguments for -d. Usage: phonebook.sh -d <last> <first>"
+    exit 1
+else
+    # If the user has chosen to delete a record, delete the record from phonebook.dat
+    if [ "$1" = "-d" ]
+    then
+        shift 1
+        last=$1
+        shift 1
+        first=$1
+        # If the record does not exist in phonebook.dat, print an error and exit
+        if ! grep -q "$last $first" phonebook.dat
+        then
+            echo "Error: Record '$last $first' does not exist in phonebook.dat"
+            exit 1
+        fi
+        # Create a temporary file to store the records that are not to be deleted
+        touch phonebook.dat.tmp
+        # Copy all records that are not to be deleted to the temporary file
+        grep -v "$last $first" phonebook.dat > phonebook.dat.tmp
+        # Copy the temporary file back to phonebook.dat
+        cp phonebook.dat.tmp phonebook.dat
+        # Remove the temporary file
+        rm phonebook.dat.tmp
+        exit 0
+    fi
+fi
+
+#If a user wants to modify a record, ensure that there is enough information to do so <last> <first> <address>
+if [ "$1" = "-m" ] && [ $# -lt 4 ]
+then
+    echo "Error: Inappropriate use of phonebook.sh, not enough arguments for -m. Usage: phonebook.sh -m <last> <first> <address>"
+    exit 1
+else
+    # If the user has chosen to modify a record, modify the record in phonebook.dat
+    if [ "$1" = "-m" ]
+    then
+        shift 1
+        last=$1
+        shift 1
+        first=$1
+        shift 1
+        address="$*"
+        # If the record does not exist in phonebook.dat, print an error and exit
+        if ! grep -q "$last $first" phonebook.dat
+        then
+            echo "Error: Record '$last $first' does not exist in phonebook.dat"
+            exit 1
+        fi
+        # Create a temporary file to store the records that are not to be modified
+        touch phonebook.dat.tmp
+        # Copy all records that are not to be modified to the temporary file
+        grep -v "$last $first" phonebook.dat > phonebook.dat.tmp
+        # Append the modified record to the temporary file
+        echo "$last $first `grep "$last $first" phonebook.dat | awk '{print $3}'` $address" >> phonebook.dat.tmp
+        # Copy the temporary file back to phonebook.dat
+        cp phonebook.dat.tmp phonebook.dat
+        # Remove the temporary file
+        rm phonebook.dat.tmp
+        # Sort the phonebook.dat file, first priority is last name, second priority is first name
+        sort +0 -1 +1 -2 phonebook.dat -o phonebook.dat
+        exit 0
+    fi
+fi
+
+# If the user has chosen to print records, ensure that there is enough information to do so <pattern>
+if [ "$1" = "-p" ] && [ $# -lt 2 ]
+then
+    echo "Error: Inappropriate use of phonebook.sh, not enough arguments for -p. Usage: phonebook.sh -p <pattern>"
+    exit 1
+else
+    # If the user has chosen to print records, print the records that match the pattern
+    if [ "$1" = "-p" ]
+    then
+        shift 1
+        pattern=$1
+        # If no records match the pattern, print an error and exit
+        if ! grep -q "$pattern" phonebook.dat
+        then
+            echo "Error: No records match the pattern '$pattern'"
+            exit 1
+        fi
+        # Print the records that match the pattern
+        grep "$pattern" phonebook.dat
+        exit 0
+    fi
+fi
+
+
 
 
 
---
+++ plot.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Author: Brady Henderson
+# Assignment 4: Create a Plot Utility
+# Purpose: This script allows a user to plot integer values in a bar chart as a histogram.
+#          The way to run the script is as follows `plot.sh <filename> <fieldname>`. The filename
+#          is the name of a file that contains the data to be plotted, with the first line being the name for 
+#          the field. The fieldname is the name of the field in the file that contains the data to be plotted.
+#          This script will help me learn the use of head, tail, wc, cut, sed, gnuplot, ps2pdf, and evince unix utilities.
+
+
+# Check if the user has passed more than two arguments
+if [ $# -ne 2 ]
+then
+    echo "Error: Inappropriate use of plot.sh. Usage: plot.sh <filename> <fieldname>"
+    exit 1
+fi
+
+# Check if the file exists
+if [ ! -f $1 ]
+then
+    echo "Error: File $1 does not exist."
+    exit 1
+fi
+
+# Check if the fieldname exists in the file
+if ! head -n 1 $1 | grep -q $2
+then
+    echo "Error: Fieldname $2 does not exist in file $1."
+    exit 1
+fi
+
+
