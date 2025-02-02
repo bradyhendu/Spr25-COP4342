333fff64a8b7dca3af9656ee870544b9c81f0d11
Brady Henderson <bradyhendu@gmail.com>
2025-02-02 12:42:53 -0500

    DONE

--- plot.sh
+++ plot.sh
@@ -30,11 +30,60 @@ then
     exit 1
 fi
 
-# Extract all the integer values in the specified column into a seperate file
-cut -d ' ' -f $2 $1 | tail -n +2 | sed 's/[^0-9]*//g' > temp1.dat
+# Determine the field number of the specified field name
+field_num=$(head -n 1 $1 | tr ' ' '\n' | nl -v 0 | grep -w $2 | awk '{print $1}')
 
+# Extract all the integer values in the specified column into a separate file
+cut -d ' ' -f $((field_num + 1)) $1 | tail -n +2 | sed 's/[^0-9]*//g' > temp1.dat
 # Sort the integer values in ascending order
 sort -n temp1.dat > temp2.dat
+
+# Extract the first and last integer values
+min=$(head -n 1 temp2.dat)
+max=$(tail -n 1 temp2.dat)
+
+# Transform the file so each row has two fields, first the unique integer value and second the frequency of the value
+uniq -c temp2.dat | awk '{print $2, $1}' > temp3.dat
+
+# Find the maximum frequency among the different values in the file
+max_freq=$(awk '{print $2}' temp3.dat | sort -n | tail -n 1)
+
+# Copy the /home/faculty/whalley/cop4342exec/plot.p file to your current directory
+cp /home/faculty/whalley/cop4342exec/plot.p .
+
+# Modify the plot file by substituting all occurances of LOWX with the smallest value
+sed -i "s/LOWX/$min/g" plot.p
+
+# Modify the plot file by substituting all occurances of HIGHX with the largest value
+sed -i "s/HIGHX/$max/g" plot.p
+
+# Modify the plot file by substituting all occurances of HIGHY with the maximum frequency
+sed -i "s/HIGHY/$max_freq/g" plot.p
+
+# Modify the plot file by substituting all occurances of FILE with the name of the file containing the distinct vlaues
+sed -i "s/FILE/temp3.dat/g" plot.p
+
+# Run gnuplot to create a histogram
+gnuplot plot.p
+
+# Convert the histogram to a pdf file
+ps2pdf graph.ps graph.pdf
+
+# Remove all the temporary files
+rm temp1.dat temp2.dat temp3.dat plot.p graph.ps
+
+# Print a message to indicate successful plotting
+echo $2 "values in file" $1 "have been plotted successfully."
+
+# Check if the DISPLAY environment variable is set
+if [ -n "$DISPLAY" ]; then
+    # Open the pdf file if a display is available
+    evince graph.pdf &
+else
+    echo "No display available. Please open graph.pdf manually."
+fi
+
+exit 0
 
 
 
