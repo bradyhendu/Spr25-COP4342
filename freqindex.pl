c1f381e1f12b7a0eb36dd0a1a9d03b41ca798871
Brady Henderson <bradyhendu@gmail.com>
2025-03-06 08:41:06 -0500

    freqindex.pl is ready to turn in

--- freqindex.pl
+++ freqindex.pl
@@ -8,5 +8,48 @@ use strict;
 #          The frequency index is a list of words and the number of times they appear in the input, sorted by frequency in descending order.
 #          The words are case-insensitive and punctuation is removed. The words should be printed in alphabetical order.
 
+# Read all input into an array
+my @input = <>;
+chomp @input;
 
+# Create a hash to store the frequency of each word
+my %word_freq;
 
+# Iterate through the input array and store the frequency of each word in the hash
+foreach my $line (@input) {
+    # Remove punctuation and convert to lowercase
+    $line =~ s/[[:punct:]]//g;
+    $line = lc $line;
+
+    # Split the line into words
+    my @words = split /\s+/, $line;
+
+    # Count the frequency of each word
+    foreach my $word (@words) {
+        $word_freq{$word}++;
+    }
+}
+
+# Group the words by frequency
+my %freq_words;
+foreach my $word (keys %word_freq) {
+    my $freq = $word_freq{$word};
+    push @{$freq_words{$freq}}, $word;
+}
+
+print "frequency index:\n";
+print "----------------\n";
+
+# Print the frequency index in descending order
+foreach my $freq (sort {$b <=> $a} keys %freq_words) {
+    my @words = sort @{$freq_words{$freq}};
+    my $line = sprintf("%2d: ", $freq) . join(", ", @words);
+
+    while(length($line) > 80){
+        my $break_pos = rindex($line, ", ", 80);
+        $break_pos = 80 if $break_pos == -1;
+        print substr($line, 0, $break_pos + 2) . "\n";
+        $line = "    ". substr($line, $break_pos + 2);
+    }
+    print "$line\n";
+}
